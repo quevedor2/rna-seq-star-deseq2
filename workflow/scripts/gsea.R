23e47906547235a8a64eb7671f745e9ae1a80dd0
Rene Quevedo <>
2021-11-22 15:52:03 -0500

    Added msigdb GSEA and over-represwentation analysis instead of GO and KEGG, and added a second ggplot visualization of pca plot

--- workflow/scripts/gsea.R
+++ workflow/scripts/gsea.R
@@ -11,17 +11,20 @@ library("GOstats")
 library("clusterProfiler")
 library("enrichplot")
 library("ggplot2")
+library("msigdbr")
 
 
 # Create a reference map of ENSEMBL to SYMBOL
 if(snakemake@params[['genome']] == 'mus_musculus'){
     library("org.Mm.eg.db")
     annotation <- "org.Mm.eg.db"
+    species <- 'Mus musculus'
     genome <- org.Mm.eg.db
     org_code <- 'mmu'
 } else if(snakemake@params[['genome']] == 'homo_sapiens'){
     library("org.Hs.eg.db")
     annotation <- "org.Hs.eg.db"
+    species <- 'Homo sapiens'
     genome <- org.Hs.eg.db
     org_code <- 'hsa'
 } else {
@@ -32,127 +35,93 @@ gene_ids <- mapIds(genome, keys=txby, column='ENTREZID',
                    keytype='ENSEMBL', multiVals="first")
 
 ## Read gene expr and DEG
-res <- read.table(snakemake@input[[1]], sep="\t",
+res <- read.table(input, sep="\t",
                   header=TRUE, stringsAsFactors = FALSE, check.names = FALSE)
 rownames(res) <- res$gene
 
 ## Select the significant genes and filters
-resSigind = res[ which(res$padj < as.numeric(as.character(snakemake@params[["maxp"]])) &
-                         res$log2FoldChange > 0 &
+resSig = res[ which(res$padj < as.numeric(as.character(snakemake@params[["maxp"]])) &
+                         abs(res$log2FoldChange) > 0 &
                          res$baseMean > as.numeric(as.character(snakemake@params[["minbase"]]))), ]
-resSigrep = res[ which(res$padj < as.numeric(as.character(snakemake@params[["maxp"]])) &
-                         res$log2FoldChange < 0 &
-                         res$baseMean > as.numeric(as.character(snakemake@params[["minbase"]]))), ]
-resSig = rbind(resSigind, resSigrep)
 resFilt <- res[res$baseMean > as.numeric(as.character(snakemake@params[["minbase"]])),]
 
-## GO ontology enrichment analysis
-if(nrow(resSig) > 0 & nrow(resFilt) > 0){
-  print("GO Enrichment Analysis")
-  params=new("GOHyperGParams",
-             geneIds=unique(na.omit(gene_ids[rownames(resSig)])),
-             universeGeneIds=unique(na.omit(gene_ids[resFilt$gene])),
-             annotation=annotation,
-             ontology="BP",
-             pvalueCutoff=0.001,
-             conditional=TRUE,
-             testDirection="over")
-  overRepresented=hyperGTest(params)
-  go_summ <- summary(overRepresented)[,c(1,2,5,6,7)]
-} else {
-  print("No significant genes to run: GO Enrichment Analysis")
-  go_summ <- data.frame()
-}
-write.table(go_summ, file=snakemake@output[["go"]],
+
+msig_lvls <- list('H'=list(NULL),                       # hallmark gene sets
+                  'C1'=list(NULL),                      # positional gene sets
+                  'C2'=list('CP:REACTOME'),             # curated gene sets
+                  'C5'=list('GO:BP', 'GO:CC', 'GO:MF'), # ontology gene sets
+                  'C6'=list(NULL),                      # oncogenic signature gene sets
+                  'C7'=list('IMMUNESIGDB'),             # immunologic signature gene sets
+                  'C8'=list(NULL))                      # cell type signature gene sets
+ora_gra <- lapply(names(msig_lvls)[5:7], function(mlvl){
+  sub_ora_gra <- lapply(msig_lvls[[mlvl]], function(sublvl){
+    print(paste0(">", mlvl, ":", sublvl, "..."))
+    msig_ds <- msigdbr(species = species, category = mlvl, subcategory = sublvl) %>% 
+      dplyr::select(gs_name, entrez_gene) %>% 
+      as.data.frame()
+    
+    # overrepresentation analysis
+    sig_ora <- tryCatch({
+      enricher(gene = na.omit(gene_ids[resSig$gene]), TERM2GENE = msig_ds)@result
+    }, error=function(e){NULL})
+    
+    
+    
+    # GSEA analysis
+    lfc_v <- setNames(resFilt$log2FoldChange, 
+                      gene_ids[resFilt$gene])
+    msig_gsea <- tryCatch({
+      GSEA(sort(na.omit(lfc_v), decreasing = T), TERM2GENE = msig_ds, pvalueCutoff = 1)
+    })
+    msig_gsea_df <- as.data.frame(msig_gsea)[,1:10]
+    
+    
+    ## network of similar terms
+    n <- sum(msig_gsea@result$pvalue < 0.05)
+    if(n > 25) n <- 25
+    if(n < 5) n <- 5
+    emap <- emapplot(pairwise_termsim(msig_gsea), showCategory = n)
+    
+    ## Dotplot of upregulate and downregulated terms
+    dp <- dotplot(msig_gsea, showCategory = n, title = "Enriched Pathways" , split=".sign") +
+      facet_grid(.~.sign)
+    
+    ## GSEA-type plot of terms
+    gg_gsea <- gseaplot2(msig_gsea, geneSetID = head(msig_gsea@result$ID,n))
+    
+    ## GSEA-type plot of terms
+    ridge <- ridgeplot(msig_gsea, showCategory = n) + labs(x = "enrichment distribution")
+    
+    
+    return(list("gsea-viz"=list(emap, dp, gg_gsea, ridge),
+                "gsea-tbl"=msig_gsea_df,
+                "ora-tbl"=sig_ora))
+  })
+  return(sub_ora_gra)
+})
+ora_gra <- unlist(ora_gra, recursive = F)
+names(ora_gra) <- unlist(sapply(names(msig_lvls), function(id) paste(id, msig_lvls[[id]], sep="-")))
+
+# Plot out all levels of msigdb
+pdf(snakemake@output[["gsea_pdf"]], height = 15, width = 15)
+lapply(names(ora_gra), function(mlvl){
+  sapply(ora_gra[[mlvl]][['gsea-viz']], function(igv) {
+    try(print(igv + ggtitle(mlvl)), silent=T)
+  })
+  plot(1, type='n', axes=FALSE, xlab='', ylab='')
+})
+dev.off()
+
+# Write out the GSEA table
+gsea_tbl <- do.call(rbind, lapply(ora_gra, function(i) i$`gsea-tbl`))
+gsea_tbl <- as.data.frame(gsea_tbl) 
+gsea_tbl$msig_lvl <- gsub("^(.*?)\\..*", "\\1", rownames(gsea_tbl))
+write.table(gsea_tbl, file=snakemake@output[["gsea_tbl"]],
             sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
 
-## GSEA
-if(nrow(resFilt) > 0){
-    print("GSEA on GO terms")
-    gene_list <- resFilt$log2FoldChange
-    names(gene_list) <- resFilt$gene
-    gene_list<-sort(na.omit(gene_list), decreasing = TRUE)
-
-    gse <- tryCatch({
-      gseGO(geneList=gene_list,
-                 ont ="ALL",
-                 keyType = "ENSEMBL",
-                 minGSSize = 3,
-                 maxGSSize = 800,
-                 pvalueCutoff = 0.05,
-                 verbose = TRUE,
-                 OrgDb = genome,
-                 pAdjustMethod = "fdr")
-     }, error=function(e){NULL})
-} else {
-    print("No significant genes to run: GSEA on GO terms")
-    gse <- NULL
-}
-
-pdf(snakemake@output[["gseago_pdf"]], height = 20, width = 20)
-if(is.null(gse)){
-  write.table(data.frame(), file=snakemake@output[["gseago_table"]],
-              col.names = TRUE, row.names = TRUE, quote = FALSE)
-} else {
-  tryCatch({
-    print(emapplot(pairwise_termsim(gse), showCategory = 50))
-  }, error=function(e){warning("Could not plot GO emapplot")})
-
-  tryCatch({
-    print(ridgeplot(gse) + labs(x = "enrichment distribution"))
-  }, error=function(e){warning("Could not plot GO ridgeplot")})
-  write.table(gse@result[,1:11], file=snakemake@output[["gseago_table"]],
-              col.names = TRUE, row.names = TRUE, quote = FALSE)
-  #gseaplot(gse, by = "all", title = gse$Description[1], geneSetID = 1)
-}
-plot(1, type='n', axes=FALSE, xlab='', ylab='')
-dev.off()
-
-
-## KEGG GSEA
-if(nrow(resFilt) > 0){
-    print("GSEA on KEGG terms")
-    kegg_gene_list <- resFilt$log2FoldChange
-    names(kegg_gene_list) <- gene_ids[resFilt$gene]
-    kegg_gene_list <- kegg_gene_list[-which(is.na(names(kegg_gene_list)))]
-    kegg_gene_list<-sort(na.omit(kegg_gene_list), decreasing = TRUE)
-
-    kk2 <- tryCatch({
-        gseKEGG(geneList     = kegg_gene_list,
-                 organism     = org_code,
-                 nPerm        = 10000,
-                 minGSSize    = 3,
-                 maxGSSize    = 800,
-                 pvalueCutoff = 0.05,
-                 pAdjustMethod = "fdr",
-                 keyType       = "ncbi-geneid",
-                 use_internal_data=TRUE)
-        }, error=function(e){NULL})
-} else {
-    print("No significant genes to run: GSEA on KEGG terms")
-    kk2 <- NULL
-}
-
-pdf(snakemake@output[["gseakegg_pdf"]], height = 20, width = 20)
-if(is.null(gse)){
-    write.table(data.frame(), file=snakemake@output[["gseakegg_table"]],
-              col.names = TRUE, row.names = TRUE, quote = FALSE)
-} else {
-    tryCatch({
-      print(dotplot(kk2, showCategory = 10, title = "Enriched Pathways" , split=".sign") +
-         facet_grid(.~.sign))
-     }, error=function(e){warning("Could not plot KEGG dotplot")})
-
-    tryCatch({
-        print(emapplot(pairwise_termsim(kk2), showCategory = 50))
-     }, error=function(e){warning("Could not plot KEGG emapplot")})
-
-    tryCatch({
-    print(ridgeplot(kk2) + labs(x = "enrichment distribution"))
-     }, error=function(e){warning("Could not plot KEGG ridgeplot")})
-
-    write.table(kk2@result[,1:11], file=snakemake@output[["gseakegg_table"]],
-              col.names = TRUE, row.names = TRUE, quote = FALSE)
-}
-plot(1, type='n', axes=FALSE, xlab='', ylab='')
-dev.off()
+# Write out the over-representation analysis table
+ora_tbl <- do.call(rbind, lapply(ora_gra, function(i) i$`ora-tbl`))
+ora_tbl <- as.data.frame(ora_tbl) 
+ora_tbl$msig_lvl <- gsub("^(.*?)\\..*", "\\1", rownames(ora_tbl))
+write.table(ora_tbl, file=snakemake@output[["ora_tbl"]],
+            sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
